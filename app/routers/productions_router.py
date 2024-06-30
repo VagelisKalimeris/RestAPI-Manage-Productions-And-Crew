f3475087d611c8651f8181ae1689ccee705b7009
Evangelos Kalimeris <vageliskalimeris@gmail.com>
2024-06-30 22:20:10 +0200

    Fix prod returns.

--- app/routers/productions_router.py
+++ app/routers/productions_router.py
@@ -12,7 +12,8 @@ from app.routers.router_dependencies import get_db
 from app.models.shared.shared_models import PrettyJSONResponse, Error
 from app.models.route.pagination_models import PaginationResult
 from app.models.route.productions_models import ProductionDetails, SortProductionsBy, NewProdDates, \
-    AllProductionsResult, ProductionResult, NewProductionResult, UpdateProductionResult, CancelProductionResult
+    AllProductionsResult, ProductionResult, NewProductionResult, UpdateProductionResult, CancelProductionResult, \
+    Production
 
 router = APIRouter()
 
@@ -21,7 +22,8 @@ router = APIRouter()
 def retrieve_all_scheduled_productions(min_date: date | None = date.min, max_date: date | None = date.max,
                                        title: str = None, sort_by: SortProductionsBy = None,
                                        page_size: Annotated[int, Query(ge=1)] = DEFAULT_PAGE_SIZE,
-                                       page_num: Annotated[int, Query(ge=1)] = 1, db: Session = Depends(get_db)):
+                                       page_num: Annotated[int, Query(ge=1)] = 1, db: Session = Depends(get_db)) \
+        -> AllProductionsResult:
     """
     Returns all scheduled productions details. Optional start/end dates filter results to given time range.
     """
@@ -30,12 +32,27 @@ def retrieve_all_scheduled_productions(min_date: date | None = date.min, max_dat
                                                          (page_num - 1) * page_size), Error):
         raise HTTPException(status_code=prods.status, detail=prods.message)
 
-    return AllProductionsResult('Details for scheduled productions were successfully retrieved.', prods[0],
-                                PaginationResult(prods[1], page_num, ceil(prods[1] / page_size))).__dict__
+    return AllProductionsResult(
+        message='Details for scheduled productions were successfully retrieved.',
+        data=[
+            Production(
+                title=prod.title,
+                start=prod.start,
+                end=prod.end,
+                id=prod.id
+            )
+            for prod in prods[0]
+        ],
+        pagination=PaginationResult(
+            total_records=prods[1],
+            current_page=page_num,
+            total_pages=ceil(prods[1] / page_size)
+        )
+    )
 
 
 @router.get('/productions/{prod_id}', status_code=200, response_class=PrettyJSONResponse)
-def retrieve_scheduled_production(prod_id: int, db: Session = Depends(get_db)):
+def retrieve_scheduled_production(prod_id: int, db: Session = Depends(get_db)) -> ProductionResult:
     """
     Returns specific scheduled production details.
     """
@@ -43,11 +60,19 @@ def retrieve_scheduled_production(prod_id: int, db: Session = Depends(get_db)):
         # Check for errors
         raise HTTPException(status_code=prod.status, detail=prod.message)
 
-    return ProductionResult('Details for production were successfully retrieved.', prod).__dict__
+    return ProductionResult(
+        message='Details for production were successfully retrieved.',
+        data=Production(
+            title=prod.title,
+            start=prod.start,
+            end=prod.end,
+            id=prod.id
+        )
+    )
 
 
 @router.post('/productions', status_code=201, response_class=PrettyJSONResponse)
-def create_production(prod_details: ProductionDetails, db: Session = Depends(get_db)):
+def create_production(prod_details: ProductionDetails, db: Session = Depends(get_db)) -> NewProductionResult:
     """
     Registers given production.
     """
@@ -56,11 +81,14 @@ def create_production(prod_details: ProductionDetails, db: Session = Depends(get
                                                   prod_details.crew_reqs), Error):
         raise HTTPException(status_code=new_prod.status, detail=new_prod.message)
 
-    return NewProductionResult('Production was successfully scheduled.', new_prod).__dict__
+    return NewProductionResult(
+        message='Production was successfully scheduled.',
+        new_prod_id=new_prod
+    )
 
 
 @router.patch('/productions/{prod_id}', status_code=200, response_class=PrettyJSONResponse)
-def update_production(prod_id: int, new_dates: NewProdDates, db: Session = Depends(get_db)):
+def update_production(prod_id: int, new_dates: NewProdDates, db: Session = Depends(get_db)) -> UpdateProductionResult:
     """
     Updates given production's dates, as long as no crew scheduling conflicts arise.
     """
@@ -69,11 +97,13 @@ def update_production(prod_id: int, new_dates: NewProdDates, db: Session = Depen
                                                           new_dates.new_end), Error):
         raise HTTPException(status_code=updated_prod.status, detail=updated_prod.message)
 
-    return UpdateProductionResult(f'Production dates for show: \'{prod_id}\', were successfully updated.').__dict__
+    return UpdateProductionResult(
+        message=f'Production dates for show: \'{prod_id}\', were successfully updated.'
+    )
 
 
 @router.delete('/productions/{prod_id}', status_code=200, response_class=PrettyJSONResponse)
-def cancel_production(prod_id: str, db: Session = Depends(get_db)):
+def cancel_production(prod_id: str, db: Session = Depends(get_db)) -> CancelProductionResult:
     """
     Deletes given production.
     """
@@ -81,4 +111,6 @@ def cancel_production(prod_id: str, db: Session = Depends(get_db)):
     if isinstance(deleted_prod := delete_existing_production(db, prod_id), Error):
         raise HTTPException(status_code=deleted_prod.status, detail=deleted_prod.message)
 
-    return CancelProductionResult(f'Production with id: \'{prod_id}\' was successfully cancelled.').__dict__
+    return CancelProductionResult(
+        message=f'Production with id: \'{prod_id}\' was successfully cancelled.'
+    )
